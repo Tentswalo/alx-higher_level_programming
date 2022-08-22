94b2f709af1289fc8fe148cc70e74e41e994bdb5
Michelle <mpenane1@gmail.com>
2022-08-22 03:09:14 -0700

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Phyton is an interpreted, interative, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[38:65] + str[104:110] + str[:6]
+print(str)
