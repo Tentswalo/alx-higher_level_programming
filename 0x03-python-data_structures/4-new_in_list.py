a282bd5941c16e042a9611cc6a176cc4c47df962
Michelle <mpenane1@gmail.com>
2022-08-28 23:14:50 -0700

    data

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,4 +3,5 @@ def new_in_list(my_list, idx, element):
     dup = my_list[:]
     if idx >= 0 and idx < len(dup):
         dup[idx] = element
-        return dup
+        return (dup)
+    return (my_list)
