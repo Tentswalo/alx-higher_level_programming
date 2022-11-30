437bb3065923e61b34c823fd1fee04a189809172
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2022-11-30 19:24:06 +0200

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // JS to check arguments passed into script
 
-const process = require('process');
-if (process.argv.length === 2) {
+if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
