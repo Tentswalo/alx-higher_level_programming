2e9ebe506b2541028b1383d4a605eef1ba583f3a
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2024-04-09 23:53:30 +0200

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
-
-#!/usr/bin/node
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
