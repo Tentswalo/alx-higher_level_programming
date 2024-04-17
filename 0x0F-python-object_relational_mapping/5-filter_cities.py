194e36fe78581f1762ffbbf81558f83e8606a00b
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2024-04-18 00:43:51 +0200

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,48 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
- takes in the name of a state as an argument
- and lists all cities of that state, using the
-database hbtn_0e_4_usa
+@description:
+    This module provides a simple connection to
+    a Mysql database with MySQLdb module, using
+    the comand line arguments.
+
+    Takes in the name of a state as an argument
+    and lists all cities of that state, using
+    the database given, safe from sql
+    injections.
 """
-import sys
-import MySQLdb
 
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-def main():
-    conn = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8"
-                            )
-    state_name = sys.argv[4]
-    cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (state_name,))
-    row = cur.fetchall()
-    re = ""
-    for r in row:
-        re += r[0] + ", "
-    print(re[0:-2:])
-    cur.close()
-    conn.close()
+    try:
+        db_connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+    except MySQLdb.OperationalError:
+        print("Can't connect to database")
 
+    cursor = db_connection.cursor()
 
-if __name__ == "__main__":
-    main()
+    # select all states with the name specified
+    query = ("SELECT cities.name " +
+             "FROM states INNER JOIN cities " +
+             "ON state_id = states.id WHERE states.name = %s")
+
+    # Make safe from sql injections.
+    safe_arg = (sys.argv[4],)
+
+    cursor.execute(query, safe_arg)
+
+    states = cursor.fetchall()
+
+    # Printing fetch data with comma separation
+    output = ", ".join(state[0] for state in states)
+    print(output)
+
+    db_connection.close()
